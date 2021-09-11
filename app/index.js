7a10800d72a8b3dc31145b3c1437c593803edd31
chenxyzj <chenxyzj@163.com>
2021-09-11 13:46:42 +0800

    node.js.yml增加docker build构造镜像的指令。

--- app/index.js
+++ app/index.js
@@ -11,8 +11,8 @@ app.get("/app1",(req,res) =>
  res.send(`APPID: ${APPID} app1's 页面.`));
 
 app.get("/app2",(req,res) => 
- res.send(`APPID: ${APPID} app2's page.`)); 
+ res.send(`APPID: ${APPID} app2's 页面.`)); 
 
  app.get("/app3",(req,res) => 
- res.send(`APPID: ${APPID} app3's page.`)); 
+ res.send(`APPID: ${APPID} app3's 页面.`)); 
 app.listen(APPID,() => console.log(`${APPID} is listening on port ${APPID}...`));
