9cac1c3d9b8122e88bb15b5b930b0d21ab6a8114
chenxyzj <chenxyzj@163.com>
2021-09-11 13:28:43 +0800

    修改index.js测试CI/CD

--- app/index.js
+++ app/index.js
@@ -8,9 +8,11 @@ app.get("/",(req,res) =>
  res.send(`APPID: ${APPID} homepage!`));
 
 app.get("/app1",(req,res) => 
- res.send(`APPID: ${APPID} app1's page.`));
+ res.send(`APPID: ${APPID} app1's 页面.`));
 
 app.get("/app2",(req,res) => 
  res.send(`APPID: ${APPID} app2's page.`)); 
 
+ app.get("/app3",(req,res) => 
+ res.send(`APPID: ${APPID} app3's page.`)); 
 app.listen(APPID,() => console.log(`${APPID} is listening on port ${APPID}...`));
