e52401621d3d7703023fa4ff2b47a9b4868eb6e9
chenxyzj <chenxyzj@163.com>
2021-09-11 13:51:49 +0800

    在index.js增加路由app4.

--- app/index.js
+++ app/index.js
@@ -13,6 +13,10 @@ app.get("/app1",(req,res) =>
 app.get("/app2",(req,res) => 
  res.send(`APPID: ${APPID} app2's 页面.`)); 
 
- app.get("/app3",(req,res) => 
+app.get("/app3",(req,res) => 
  res.send(`APPID: ${APPID} app3's 页面.`)); 
-app.listen(APPID,() => console.log(`${APPID} is listening on port ${APPID}...`));
+
+app.get("/app4",(req,res) => 
+ res.send(`APPID: ${APPID} app4's 页面.`)); 
+
+ app.listen(APPID,() => console.log(`${APPID} is listening on port ${APPID}...`));
