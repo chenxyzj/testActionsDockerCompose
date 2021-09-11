2352edc9a6e4764a7ec7c3e00bb14ee1c5563f26
chenxyzj <chenxyzj@163.com>
2021-09-11 13:58:39 +0800

    修改页面为子页面，通过push触发CI/CD。

--- app/index.js
+++ app/index.js
@@ -8,15 +8,15 @@ app.get("/",(req,res) =>
  res.send(`APPID: ${APPID} homepage!`));
 
 app.get("/app1",(req,res) => 
- res.send(`APPID: ${APPID} app1's 页面.`));
+ res.send(`APPID: ${APPID} app1's 子页面.`));
 
 app.get("/app2",(req,res) => 
- res.send(`APPID: ${APPID} app2's 页面.`)); 
+ res.send(`APPID: ${APPID} app2's 子页面.`)); 
 
 app.get("/app3",(req,res) => 
- res.send(`APPID: ${APPID} app3's 页面.`)); 
+ res.send(`APPID: ${APPID} app3's 子页面.`)); 
 
 app.get("/app4",(req,res) => 
- res.send(`APPID: ${APPID} app4's 页面.`)); 
+ res.send(`APPID: ${APPID} app4's 子页面.`)); 
 
  app.listen(APPID,() => console.log(`${APPID} is listening on port ${APPID}...`));
